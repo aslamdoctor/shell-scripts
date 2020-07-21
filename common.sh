916689699c11013692f527a7ace98ac8cb483d64
Aslam Doctor <aslam.doctor@gmail.com>
2020-07-21 22:19:51 +0530

    added killport script

--- common.sh
+++ common.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+
 alias startgulp="git clone git@github.com:aslamdoctor/gulp-boilerplate.git ./dev"
+
+killport () {
+   sudo fuser -n tcp -k $1
+   echo "Stopped port : $1"
+   return 10
+}
