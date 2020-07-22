df7b196d4b1f65c776f2d0d31a0a160ac6308d44
Aslam Doctor <aslam.doctor@gmail.com>
2020-07-22 18:31:51 +0530

    added script to backup & restore mysql db

--- mysql.sh
+++ mysql.sh
@@ -3,3 +3,11 @@
 alias startmysql="sudo service mysql start"
 alias stopmysql="sudo service mysql stop"
 alias restartmysql="sudo service mysql restart"
+
+backupmysqldb(){
+   mysqldump -u admin -padmin $1 > ./database.sql
+}
+
+restoremysqldb(){
+   mysql -u admin -padmin $1 < ./database.sql
+}
